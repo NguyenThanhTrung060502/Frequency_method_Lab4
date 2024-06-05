e18fcb9bd9d90a47cc80253a8e87e53525b86ed8
Нгуен Тхань Чунг <nguyenthanhtrung06052002@gmail.com>
2024-06-05 21:23:34 +0300

    Update part1.py

--- part1.py
+++ part1.py
@@ -8,12 +8,9 @@ pi = np.pi
 def plot_func(X, func, color, title, legend=['Source function'], labels=['t', 'f(t)']):
     ymin = min(func) 
     ymax = max(func) 
-
     ymax = ymax + 0.1 * (ymax - ymin)
     ymin = ymin - 0.1 * (ymax - ymin)
-
     plt.figure(figsize=(8, 5)) 
-
     plt.ylim(ymin, ymax)
     plt.plot(X, func.real, color=f'{color}')
     plt.xlabel(labels[0])
@@ -27,10 +24,8 @@ def plot_func(X, func, color, title, legend=['Source function'], labels=['t', 'f
 def cmp_func(X, funcs, title, legend=['Source function', 'Restored function'], labels=['t', 'f(t)']):
     ymin = min([min(func) for func in funcs])
     ymax = max([max(func) for func in funcs])
-
     ymax = ymax + 0.1 * (ymax - ymin)
     ymin = ymin - 0.1 * (ymax - ymin)
-
     plt.figure(figsize=(8, 5)) 
     colors = ['black', 'red', 'cyan']
     plt.ylim(ymin, ymax)
@@ -41,7 +36,6 @@ def cmp_func(X, funcs, title, legend=['Source function', 'Restored function'], l
         plt.legend(legend, loc='upper right')
         plt.title(title)
         plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
-
 
 # Vẽ biến đổi Fourier của hàm số f(t) 
 def plot_image(X, func, title):
@@ -68,28 +62,18 @@ def dotProduct(X, f, g):
     dx = X[1] - X[0]
     return np.dot(f, g) * dx
 
-
 get_fourier_image = lambda X, V, func: np.array([1 / (np.sqrt(2 * pi)) * dotProduct(X, func, (lambda t: np.e ** (-1j * image_clip * t))(X)) for image_clip in V])
 get_fourier_function = lambda X, V, image: np.array([1 / (np.sqrt(2 * pi)) * dotProduct(V, image, (lambda t: np.e ** (1j * x * t))(V)) for x in X])
-
-# get_func = lambda a, t1, t2: np.vectorize(lambda t: a if t1 <= t <= t2 else 0, otypes=[complex])
-
 noised = lambda X, func, a: func + a * (np.random.rand(X.size) - 0.5) 
+
 # найти численную производную от зашумлённого сигнала
 numerical_derivative = lambda X, func: np.array([0 if i == 0 else (func[i] - func[i - 1]) / (X[i] - X[i - 1]) for i in range(len(func))])
 
 
-
-# get_first_order_filter = lambda T: tf2zpk([0, 1], [T, 1]) 
-# get_second_order_filter = lambda T1, T2, T3: tf2zpk([T1 ** 2, 2 * T1, 1], [T2 * T3, T2 + T3, 1])
-
-
 def spectral_diff(T):
     t = np.linspace(-100, 100, 10000) # array with arguments for functions
-
     sin = np.sin(t)
     noised_sin = noised(t, sin, 0.2)
-
     plot_func(t, noised_sin, color='black', title='Зашумлённый сигнал', legend=['f(t) = sin(t)'])
 
     # найти численную производную от зашумлённого сигнала
@@ -112,9 +96,7 @@ def spectral_diff(T):
     cmp_func(t, funcs=[noised_sin_derivative, restored_noised_sin_derivative, cos], title='Сравнение истинной производной cos(t) с численной и спектральной производной.', legend=['численная производная', 'Спектральная производная', 'cos(t)'])
     
 
-## FIRST TASK 
-
-spectral_diff(T=10)
+# spectral_diff(T=10)
 # spectral_diff(T=20)
 
 plt.show()
